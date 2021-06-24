d8e98ad0d2a761b9bab05a756aaa35811fcac03c
Michał Maćkowiak <48453998+Maklowic@users.noreply.github.com>
2021-06-24 15:51:54 +0200

    Add files via upload

---
+++ html/hierarchy.js
@@ -0,0 +1,10 @@
+var hierarchy =
+[
+    [ "QMainWindow", null, [
+      [ "MainWindow", "class_main_window.html", null ],
+      [ "connection", "classconnection.html", null ],
+      [ "mapped_path", "classmapped__path.html", null ],
+      [ "sensor_state", "classsensor__state.html", null ],
+      [ "wheel_speed1", "classwheel__speed1.html", null ]
+    ] ]
+];
